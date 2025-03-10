3b7c08e7db18e6092047a896bf0d9150cec48acb
Lai_lai <aoichigo77@icloud.com>
2025-03-11 02:05:45 +0900

    切り出したついでに構成も変えた

---
+++ loseface/loseface.js
@@ -0,0 +1,19 @@
+const ring = new Audio('sound/bell.mp3');
+let count = 0;
+let ringcount = document.getElementById('r0');
+
+function countplus() {
+    if (count >= 9999999) {
+        count = 9999999;
+        return;
+    }
+    count++;
+    ringcount.innerHTML = count;
+
+    let makeTweet = "私は顔を "+count+" 回無くしました\n\nhttps://lai-lai.info/loseface"
+
+    const share_twitter = document.getElementById("js-share-twitter");
+    share_twitter.setAttribute(
+    "href",
+    "https://twitter.com/share?" + "&text=" +encodeURIComponent(makeTweet));
+}
