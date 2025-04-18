72d77d77d1a962360689f987f299b81a5f25de35
Lai_lai <aoichigo77@icloud.com>
2025-04-18 14:54:10 +0900

    とりあえずコミット

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Header } from './component/Header'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <div className="main">
+        <article>
+            <h2>Top page</h2>
+            <p>らいらいのページへようこそ！</p>
+            <h3>らいらいについて</h3>
+            <img src="imgs\icons.png" alt="Me" height="75px" width="75px" />
+            <p>Y県S市出身。カワウソが好き。スープ系の料理が好き。人間となかよくなりたい。</p>
+            <h3>おともだち</h3>
+            <ul>
+                <li><a className="olink" href="Otomodachi.html">おともだち一覧</a></li>
+            </ul>
+            <h3>顔を無くすボタン</h3>
+            <ul>
+                <li><a className="olink" href="loseface.html">顔を無くすボタン</a></li>
+            </ul>
+        </article>
+    </div>
+    <footer>
+        <p className="copyright">©2025 Lai_lai</p>
+    </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header.tsx
@@ -0,0 +1,17 @@
+
+export function Header() {
+    return (
+        <>
+        <header>
+            <h1 className="head1">Lai-lai.info</h1>
+            <div className="head2">Welcome to Lai_lai's page!!</div>
+            <div className="pages">
+                <a className="page" href="index.html" >Top</a>
+                <a className="page" href="Works.html">Works</a>
+                <a className="page" href="Links.html">Links</a>
+            </div>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </header>
+        </>
+    )
+}
